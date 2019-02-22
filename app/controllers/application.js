12420dbe507248f34827c30eb6291ebc375d25e6
Ryan Toronto <ryanto@gmail.com>
2019-02-22 15:47:37 -0500

    add sandbox config

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -6,59 +6,60 @@ export default Controller.extend({
 
   actions: {
     pushWithIncludes() {
-this.store.pushPayload({
-  data: {
-    type: "posts",
-    id: 1,
-    attributes: {
-      title: "This is a test post"
-    },
-    relationships: {
-      comments: {
-        data: [
-          {
-            id: 1,
-            type: "comments"
+      this.store.pushPayload({
+        data: {
+          type: "posts",
+          id: 1,
+          attributes: {
+            title: "This is a test post"
+          },
+          relationships: {
+            comments: {
+              data: [
+                {
+                  id: 1,
+                  type: "comments"
+                }
+              ],
+              links: {
+                self: "/api/v1/posts/1/relationships/comments",
+                related: "/api/v1/posts/1/comments"
+              }
+            }
           }
-        ],
-        links: {
-          self: "/api/v1/posts/1/relationships/comments",
-          related: "/api/v1/posts/1/comments"
-        }
-      }
-    }
-  },
-  included: [
-    {
-      type: "comments",
-      id: 1,
-      attributes: {
-        title: "first comment!"
-      }
-    }
-  ]
-});
-this.target.send('refresh');
+        },
+        included: [
+          {
+            type: "comments",
+            id: 1,
+            attributes: {
+              title: "first comment!"
+            }
+          }
+        ]
+      });
+      this.target.send('refresh');
     },
 
     pushWithoutIncludes() {
-this.store.pushPayload({
-  data: {
-    type: "posts",
-    id: 1,
-    attributes: {
-      title: "This is a test post"
-    },
-    relationships: {
-      comments: {
-        links: {
-          self: "/api/v1/posts/1/relationships/comments",
-          related: "/api/v1/posts/1/comments"
+      this.store.pushPayload({
+        data: {
+          type: "posts",
+          id: 1,
+          attributes: {
+            title: "This is a test post"
+          },
+          relationships: {
+            comments: {
+              links: {
+                self: "/api/v1/posts/1/relationships/comments",
+                related: "/api/v1/posts/1/comments"
+              }
+            }
+          }
         }
-      }
-    }
-  }
-});
+      });
+      this.target.send('refresh');
     }
   }
 });
